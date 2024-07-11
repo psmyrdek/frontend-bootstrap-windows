8ce4da6a45c13004770d8a720a69a9835a168ea0
psmyrdek <przemek.smyrdek@gmail.com>
2024-07-11 14:54:23 +0200

    feat: app ver

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { useConfig } from './context/ConfigContext';
 
 const App = () => {
+  const context = useConfig();
+
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
       </div>
       <Outlet />
+      <p>Wersja aplikacji: {context.appVersion}</p>
     </div>
   );
 };
